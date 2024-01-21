9f2323b826054abc0520755dc3784956a752286b
Lena Krause <lena.krause@umontreal.ca>
2024-01-21 15:28:23 -0500

    cleaning code

--- where-is-art/sketch.js
+++ where-is-art/sketch.js
@@ -2,9 +2,10 @@
     title: Where is art? 
     author: LenaMK
     date: 2023-01-13
-    description: rechercher le texte "Art is here". La souris est un cercle blanc qui révèle le texte en noir sur fond noir. L'emplacement du texte est différent (random) à chaque fois qu'on charge la page, et se modifie quand on clique sur le texte
+    description: rechercher le texte "Art is here". La souris est un cercle blanc qui révèle le texte en noir sur fond noir. L'emplacement du texte est différent (random) à chaque fois qu'on charge la page ou quand on clique sur le texte
     inpiration: effet lampe de poche; tel un spéléologue dans une grotte. Discussions dans le cours sur qu'est-ce que c'est l'art, et "où" il se trouve
-    initial code inspired by: https://www.geeksforgeeks.org/p5-js-displaywidth-variable/
+    initial code inspired by: 
+        - https://www.geeksforgeeks.org/p5-js-displaywidth-variable/
 
 */
 
@@ -38,64 +39,44 @@ function setup() {
 
     setArt();
 
-    //the cursor could actually be the text cursor(type)
-    noCursor(); //enlève la souris, voir si c'est à garder vu qu'on ajoute une interaction avec un clic
+    noCursor(); 
 } 
 
 function setArt(){
-    // enlever l'ancier s'il y en a un et placer le nouveau
-    clear()
-    x = random(5, windowWidth-artWidth) 
-    y = random(artAscent, windowHeight-artDescent) //pour que ça fitte exactement
+    //clear previous setArt
+    clear() 
 
-    console.log("x", x)
-    console.log("y", y)
+    x = random(5, windowWidth-artWidth) 
+    y = random(artAscent, windowHeight-artDescent)
 }
 
 
-//function mousePressed() by p5
-// The coding Train https://www.youtube.com/watch?v=DEHsr4XicN8
-// dist(x,y, mouseX, mouseY)
-function mousePressed() {
-    //quand le cercle est sur Art: qz
 
-    console.log(mouseX, mouseY)
+function mousePressed() {
+
     var artCenterX = x + artWidth/2
     var artCenterY = y + artHeight/2
 
     var distance = dist(mouseX, mouseY, artCenterX, artCenterY)
-    console.log("distance from mouse", distance)
-    //make sure the new art location is far enough from current pos? 
-
+    
     if (distance < overflow)
         setArt();
     
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    setArt()
 }
 
 function draw() { 
      
     background(0o0); 
 
-
     circle(mouseX, mouseY, circleRadius); 
-    //quel support pour tablette/touch? ok mais erreur dans taille de navigateur
-
-    //rectangle is exactly the size of the text 
-    //rect(x, y-artAscent, artWidth, artHeight) 
-
-    //text gets a bit thick from reloading over itself but it doesn't when the mouse is over it so no difference
+   
     text(art, x, y); 
-
-    rect()
-
-
 
 } 
 
 
-// Pour changer la taille du canevas si la taille de la fenêtre est modifiée.
-// source: https://stackoverflow.com/questions/68029286/how-do-i-make-the-canvas-perfectly-fit-to-the-window-size-in-p5
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  setArt()
-}
