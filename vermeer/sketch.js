1cc4b4571cece1d325bf142c1dea2e0e125e02b3
Lena Krause <lena.krause@umontreal.ca>
2024-01-29 18:39:38 -0500

    some strange pattern, almost there?

--- vermeer/sketch.js
+++ vermeer/sketch.js
@@ -7,7 +7,7 @@
 
 */
 
-var side = 25; //size of square
+var side = 50; //size of square
 var diag = Math.sqrt(side*side*2)
 
 var margin = diag/2 
@@ -18,23 +18,43 @@ var margin = diag/2
 function setup() { 
     
     createCanvas(windowWidth, windowHeight); 
-    console.log("diag", diag)
     noLoop();
 } 
    
 
 function draw() { 
      
-    background(0o0); 
+    background(0o0); //white on black, could be the other way around but there is more black so should be easier
 
     fill(255);
 
     //quad(x1, y1, x2, y2, x3, y3, x4, y4)
+    
+    let line = false; 
+    //let column = false;
+    let alternate = true;
 
-    for (let y = margin; y < (windowHeight-margin); y += diag){
-        for ( let x = margin; x < (windowWidth-margin); x += diag){ 
-            quad(x, y, x+diag/2, y+diag/2, x, y+diag, x-diag/2, y+diag/2)
+    for (let y = margin; y < (windowHeight-diag); y += diag){
+        //line
+        for ( let x = margin; x < (windowWidth-diag); x += diag){ 
+            //column
+            if(line){
+                quad(x, y, x+diag/2, y+diag/2, x, y+diag, x-diag/2, y+diag/2)
+                line = false;
+            }
+            else {
+                line = true
+            }   
         }
+
+        if (alternate){
+            line = !line
+            alternate = false
+        }
+        else{
+            alternate = true
+        }
+         //reset at each line
     }
 
     
