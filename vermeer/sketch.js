6eb7946c57f7cc79b51157d5f45b83367691a44a
Lena Krause <lena.krause@umontreal.ca>
2024-01-30 16:35:19 -0500

    notes de la présentation

--- vermeer/sketch.js
+++ vermeer/sketch.js
@@ -6,16 +6,18 @@
     inpiration: genuary 25th prompt "If you like generative art, you probably have some photos on your phone of cool looking patterns, textures, shapes or things that you’ve seen. You might have even thought, “I should try to recreate this with code”. Today is the day."
 */
 
-var side = 50; //size of square
-var diag = Math.sqrt(side*side*2)
+//tesselation: pavage (escher), motifs
+
+var side = 70; //size of square
+var diag = Math.sqrt(side*side*2) //hypothénuse
+
+//possibilité de rotate un ou des éléments
+//repaire 0,0 en haut à gauche. Rotate tourne le repaire
 
 var margin = diag/2 
 
-
-
-
 function setup() { 
-    
+
     createCanvas(windowWidth, windowHeight); 
     noLoop();
 } 
@@ -23,7 +25,7 @@ function setup() {
 
 function draw() { 
      
-    background(0o0); //white on black, could be the other way around but there is more black so should be easier
+    background(0o0);
 
     fill(255);
 
@@ -49,16 +51,10 @@ function draw() {
             else {
                 quad(x, y, x+diag/2, y+diag/2, x, y+diag, x-diag/2, y+diag/2)
             }
-
-            
-             
         }
         alternate = !alternate
-        
         line = true
-    }
-
-    
+    }    
 } 
 
 
