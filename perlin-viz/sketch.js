91cd14d733fd63670a09730dcce02492a51844a6
Lena Krause <lena.krause@umontreal.ca>
2024-02-14 20:16:54 -0500

    tilestest

--- perlin-viz/sketch.js
+++ perlin-viz/sketch.js
@@ -51,7 +51,7 @@ function setup() {
         side = windowWidth/nbSquares; 
 
     diag = Math.sqrt(side*side*2) //hypothénuse
-    margin = 10
+    margin = 50
 
     var nblines = windowHeight/diag
 
@@ -60,6 +60,14 @@ function setup() {
     }
 } 
    
+
+function makeTile (x, y){
+    fill(0, 0, 0, 250);
+    stroke(0, 0, 100, 250)
+    rect(x, y, diag, diag)                                                                                                                                                                                                                                                                                                                                                                           
+    quad(x, y, x+diag/2, y+diag/2, x, y+diag, x-diag/2, y+diag/2)
+}
+
 /*suggestions
 - travailler sur le ton pour évolution
 - real tiles (no empty spaces),movement at the scale of the tile
@@ -69,13 +77,11 @@ function draw() {
     
 
     fill(0, 0, 100, 250);
-    background(0, 0, 0)
 
     var lineNb = 0
 
     yoff = 0.0
-    push()
-    //translate(windowWidth/2, windowHeight/2)
+
     for (let y = margin; y < windowHeight; y += diag){
         //line or  y height
         xoff = 0.0
@@ -85,8 +91,10 @@ function draw() {
         lineNb += 1
         //lines
         for ( let x = margin; x < windowWidth; x += diag){ 
-
+            
             xoff += incx
+            makeTile(x, y)
+            /*
             fill(linecolor[lineNb], 80, 80, 250)
             
             if (rotateCanvas){
@@ -103,47 +111,18 @@ function draw() {
             if (!tesselate){
                 break;
             }
+            */
         }
+        /*
         if (rotateCanvas){
                 angle = noise(xoff, yoff, zoff)
                 rotate(angle)
             }       
 
-
+        */
         yoff += incy
     }    
-    pop()
-    
-
-
-
-/*
-//rect for code variables
-//to have multiple canvases: instance mode 
-// https://github.com/processing/p5.js/wiki/Global-and-instance-mode
-
-    fill(0, 0, 100, 250)
-    //rectangle location
-    var rectMargin = 80
-    var rectX = windowWidth/3*2-rectMargin
-    var rectY = windowHeight/3+rectMargin
-    var rectWidth = windowWidth/3-rectMargin*2
-    var rectHeight = windowHeight/3*2-rectMargin*2
-
-    rect(rectX, rectY, rectWidth, rectHeight)
-
-    fill(0, 100, 0, 250)
-
-    for (let textboxY = rectY; textboxY < (rectY + rectHeight); textboxY += textboxHeight){
-        text(String(noise(xoff, yoff, zoff)), rectX+50, textboxY )
-
-    }
-
-
-    
-    */
-    
-    
+   
     //end of frame
     zoff += variability;
 } 
