08bdbc094acd51b981f954688ea3b35202f1f616
Lena Krause <lena.krause@umontreal.ca>
2024-03-18 18:37:46 -0400

    smoot transition of figures

--- figures-historiques/sketch.js
+++ figures-historiques/sketch.js
@@ -17,17 +17,9 @@ var minLat, minLong, maxLat, maxLong, minYear, maxYear
 var maxColor = 100
 var minColor = 10
 
-var figureTime
-var figureDistance
+var drawTime
 
-var initFigureTime = 30
-var finishedFigureTime = 150
-var drawTime, startFrame, nbSteps
-var speed = 2
-var maxDistance = 800
-//var maxStepTime = 5 //seconds
-//var maxStepFrames
-
+var transitionTime = 250
 
 var art = "\u203B"
 
@@ -42,15 +34,8 @@ function setup() {
     colorMode(HSB, 360, 100, 100, 250);
     
     createCanvas(windowWidth, windowHeight); 
-    figureTime = 120 //init figure time
-    startFrame = 0 //init start frame
-    console.log(windowWidth)
-
- //   maxDistance = Math.max(windowWidth, windowHeight)
- //   maxStepFrames = maxStepTime*60
- //   speed = maxDistance / maxStepFrames
-    
-
+    startFrame = 50
+    drawTime = 50
     data = Object.values(importedObject)
 
     //order by produced_at = from newest to oldest
@@ -69,24 +54,18 @@ function setup() {
     
     textAlign(CENTER, CENTER);
     textSize(28);     
-    
 
-    console.log(data)
-    console.log(art)
+    //console.log(data)
+    //console.log(art)
     
 } 
 
-
-
 function mousePressed(){
-
     console.log("mouse Pressed")
     if (isLooping())
         noLoop();
     else
         loop()
-
-
 }
 
 //sudo mapping to canvas
@@ -97,7 +76,6 @@ function getPosition(lat, long){
     largelong = long*1000 
 
     x = map(largelong, minLong*1000, maxLong*1000, 0, windowWidth) 
-
     y = map(largelat, minLat*1000, maxLat*1000, 0, windowHeight)
 
     return [x, y]
@@ -113,56 +91,60 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
+function drawLines(selection){
+    selection.forEach(d =>{
+        var currentLocation = getPosition(d.location.lat, d.location.lng)
+        var currentIndex = selection.indexOf(d)
+        if (currentIndex < selection.length-1){
+            
+            var nextLocation = getPosition(selection[currentIndex+1].location.lat, selection[currentIndex+1].location.lng)
+
+            line(currentLocation[0], currentLocation[1], nextLocation[0], nextLocation[1])
+
+        }
+    })
+}
+
+function drawArtSelection(selection){
+    selection.forEach(d => {
+        var currentLocation = getPosition(d.location.lat, d.location.lng)
+        noStroke()
+        fill(341, colorScale(d.produced_at), 67, 150)
+        text(art, currentLocation[0], currentLocation[1])
+    })
+}
+
 function drawSelection(selection, start){
 
     //always show the selection with more alpha
-    selection.forEach(d => {
-        noStroke()
-        fill(341, colorScale(d.produced_at), 67, 150)
-        text(art, d.artLocation[0], d.artLocation[1])
-    })
+    drawArtSelection(selection)
 
     var status = frameCount-start
-    drawTime = figureTime-initFigureTime-finishedFigureTime
+        
+    if (status > (drawTime - transitionTime)) { 
+        //reduce alpha
+        var decreaseTime = drawTime - status
+        console.log(decreaseTime)
 
-    
-    stroke(0, 0, 100, 165)
+        var currentOpacity = map(transitionTime - decreaseTime, 0, transitionTime, 0, 180)
 
-    if (status > (initFigureTime + drawTime)) { 
-        //if the entire drawing is completed, last step is to stay as is 
-        console.log("drawing completed")
-        selection.forEach(d =>{
-            if (d.nextArtLocation)
-                line(d.artLocation[0], d.artLocation[1], d.nextArtLocation[0], d.nextArtLocation[1])                 
-        })
+        stroke(0, 0, 100, 180-currentOpacity)
+        drawLines(selection);
+
 
     }
-    else if ( status > initFigureTime){ 
-        //start drawing after initial step of drawing with more alpha (Delay by initFigureTime)
+    else if ( status > transitionTime){ 
+        //Show full alpha
+        stroke(0, 0, 100, 180)
+        drawLines(selection);
         
-        var currentStep = 0
-        var nextStepTime = 0
-        for (i = 0; i < nbSteps; i++){
-            nextStepTime += selection[i].time
-            if ((status-initFigureTime) > nextStepTime){
-                currentStep += 1
-            }
-            
+    }
+    else {
+        //start appearing
+        var currentOpacity = map(status, 0, transitionTime, 0, 180)
+        stroke(0, 0, 100, currentOpacity)
+        drawLines(selection);
 
-            if (i < currentStep){
-                //draw the whole line
-                line(selection[i].artLocation[0], selection[i].artLocation[1], selection[i].nextArtLocation[0], selection[i].nextArtLocation[1])
-            }
-            else if (i == currentStep) {
-                //compute which part we need to draw
-                var percentageOfCurrentStep = (status - initFigureTime ) / nextStepTime
-
-                var endX = map(percentageOfCurrentStep, 0, 1, selection[i].artLocation[0], selection[i].nextArtLocation[0])
-                var endY = map(percentageOfCurrentStep, 0, 1, selection[i].artLocation[1], selection[i].nextArtLocation[1])
-                line(selection[i].artLocation[0], selection[i].artLocation[1], endX, endY)
-
-            }
-        }
     }
 
 }
@@ -186,7 +168,7 @@ function draw() {
     });
 
     //new selection
-    if (frameCount-figureTime == startFrame){
+    if (frameCount-drawTime == startFrame){
         startFrame = frameCount;
         var nbArtworks = Math.round( random(minArtworks, maxArtworks))
         var randomStart = Math.round( random(0, 276-nbArtworks))
@@ -197,50 +179,9 @@ function draw() {
 
         nbSteps = (selection.length-1)
         console.log("nb Steps", nbSteps)
+
+        drawTime = 800
         
-        var distance = 0;
-        var time = 0
-        selection.forEach(d =>{
-            var location = getPosition(d.location.lat, d.location.lng)
-            d.artLocation = location
-            var currentDistance
-            var currentTime
-            var current = selection.indexOf(d)
-            if (current < selection.length-1){
-                
-                var nextLocation = getPosition(selection[current+1].location.lat, selection[current+1].location.lng)
-                d.nextArtLocation = nextLocation
-                currentDistance = dist(location[0], location[1], nextLocation[0], nextLocation[1])
-                
-                if (currentDistance > maxDistance){
-                    console.log("step "+ current + " :" + currentDistance + "replaced by max distance")
-                    distance += maxDistance
-                    currentTime = Math.trunc(maxDistance / speed)
-                    
-                }
-                else {
-                    console.log("step "+ current + " :" + currentDistance)
-                    distance += currentDistance
-                    currentTime = Math.trunc(currentDistance / speed)
-                    
-                }
-                d.time = currentTime
-                time += currentTime
-                d.distance = currentDistance
-            }
-            else{
-                d.distance = 0
-                d.time = 0
-            }
-            
-        })
-
-        figureDistance = distance
-
-        console.log("Figure distance", figureDistance)
-    
-        figureTime = Math.trunc(time) + initFigureTime + finishedFigureTime
-        console.log("Figure time", figureTime)
     }
 
     if (selection)
